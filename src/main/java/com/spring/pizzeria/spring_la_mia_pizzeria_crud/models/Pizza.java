b2a6be150e45994d8e826a94b8f8ccfa72a92d29
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-21 13:44:03 +0200

    rename cartelle

--- src/main/java/com/spring/pizzeria/spring_la_mia_pizzeria_crud/models/Pizza.java
+++ src/main/java/com/spring/pizzeria/spring_la_mia_pizzeria_crud/models/Pizza.java
@@ -1,4 +1,4 @@
-package com.spring.pizzeria.spring_la_mia_pizzeria_crud.model;
+package com.spring.pizzeria.spring_la_mia_pizzeria_crud.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
