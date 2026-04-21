d7ed3509157fbab86aa64c99dc445c369362e683
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-21 14:17:58 +0200

    vengono ora visualizzate tutte le pizze in formato stringa

--- src/main/java/com/spring/pizzeria/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
+++ src/main/java/com/spring/pizzeria/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
@@ -1,16 +1,30 @@
 package com.spring.pizzeria.spring_la_mia_pizzeria_crud.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.pizzeria.spring_la_mia_pizzeria_crud.models.Pizza;
+import com.spring.pizzeria.spring_la_mia_pizzeria_crud.repositories.PizzaRepository;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequestMapping("/pizzas")
 public class PizzaController {
 
+    @Autowired
+    private PizzaRepository repo;
+
     @GetMapping("/index")
-    public String index() {
+    public String index(Model model) {
+        List<Pizza> result = repo.findAll();
+        String pizzasString = result.toString();
+
+        model.addAttribute("pizzas", pizzasString);
         return "pizzas/index";
     }
     
