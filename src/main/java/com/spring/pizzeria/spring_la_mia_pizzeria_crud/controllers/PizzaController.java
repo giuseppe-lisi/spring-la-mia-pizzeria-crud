8adf45bcf9135afb92df9252b3542e02a27b8bbb
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-21 13:44:46 +0200

    creato controller per mappare le richieste inerenti le pizze

---
+++ src/main/java/com/spring/pizzeria/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
@@ -0,0 +1,10 @@
+package com.spring.pizzeria.spring_la_mia_pizzeria_crud.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pizza")
+public class PizzaController {
+
+}
