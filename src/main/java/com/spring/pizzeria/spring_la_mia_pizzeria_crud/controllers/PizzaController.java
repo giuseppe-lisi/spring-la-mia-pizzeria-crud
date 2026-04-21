62c45d9f51dadb0a6d0a7963ccad5280769231a6
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-21 13:54:52 +0200

    aggiunta rotta index alle pizze per mostrare la lista delle pizze

--- src/main/java/com/spring/pizzeria/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
+++ src/main/java/com/spring/pizzeria/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
@@ -1,10 +1,17 @@
 package com.spring.pizzeria.spring_la_mia_pizzeria_crud.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping("/pizza")
+@RequestMapping("/pizzas")
 public class PizzaController {
 
+    @GetMapping("/index")
+    public String index() {
+        return "pizzas/index";
+    }
+    
 }
