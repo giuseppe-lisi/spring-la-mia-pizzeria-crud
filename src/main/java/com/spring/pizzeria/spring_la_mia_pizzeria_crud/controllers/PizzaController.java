9821a50526ff5fab8a721d46feef8ff4dde3c198
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-21 21:16:31 +0200

    aggiunta al controller una rotta per restituire la view del dettaglio della singola pizza

--- src/main/java/com/spring/pizzeria/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
+++ src/main/java/com/spring/pizzeria/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
@@ -11,6 +11,9 @@ import com.spring.pizzeria.spring_la_mia_pizzeria_crud.models.Pizza;
 import com.spring.pizzeria.spring_la_mia_pizzeria_crud.repositories.PizzaRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("/pizzas")
@@ -19,7 +22,7 @@ public class PizzaController {
     @Autowired
     private PizzaRepository repo;
 
-    @GetMapping("/index")
+    @GetMapping("/")
     public String index(Model model) {
         List<Pizza> result = repo.findAll();
 
@@ -27,5 +30,15 @@ public class PizzaController {
         
         return "pizzas/index";
     }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable int id, Model model) {
+        Pizza pizza = repo.findById(id).get();
+
+        model.addAttribute(pizza);
+
+        return "pizzas/show";
+    }
+    
     
 }
